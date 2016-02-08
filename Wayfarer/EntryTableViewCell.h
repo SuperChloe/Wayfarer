0f53405b3b1ede8ec2301e7130cc02f5ba92aaa1
Chloe Horgan <super.chloe@gmail.com>
2016-02-08 13:29:01 -0500

    Realm cocoapods, initial set up

---
+++ Wayfarer/EntryTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  EntryTableViewCell.h
+//  Wayfarer
+//
+//  Created by Chloe on 2016-02-08.
+//  Copyright © 2016 Chloe Horgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EntryTableViewCell : UITableViewCell
+
+@end
---
+++ Wayfarer/EntryTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  EntryTableViewCell.m
+//  Wayfarer
+//
+//  Created by Chloe on 2016-02-08.
+//  Copyright © 2016 Chloe Horgan. All rights reserved.
+//
+
+#import "EntryTableViewCell.h"
+
+@implementation EntryTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
