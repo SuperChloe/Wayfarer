326e658c5b0049b506a80dac3cbef4e78a219294
Chloe Horgan <super.chloe@gmail.com>
2016-02-09 19:31:31 -0500

    Mockups for readme and podfile

--- Wayfarer/CreateTableViewCell.m
+++ Wayfarer/CreateTableViewCell.m
@@ -11,13 +11,11 @@
 @implementation CreateTableViewCell
 
 - (void)awakeFromNib {
-    // Initialization code
+    
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
 }
 
 @end
